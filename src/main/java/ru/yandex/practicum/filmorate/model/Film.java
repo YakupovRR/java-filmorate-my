ef656bdf9fc90bfe0a554f09365bffb083faa375
Ruslan <ruslansamara00@mail.ru>
2022-08-17 22:28:21 +0400

    'Вторая загрузка'

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import javax.validation.constraints.Size;
 
 
 @Data
@@ -9,7 +8,6 @@ public class Film {
     private int id;
     @NonNull
     private String name;
-    @Size (min=1, max=200)
     private String description;
     private LocalDate releaseDate;
     private int duration;
