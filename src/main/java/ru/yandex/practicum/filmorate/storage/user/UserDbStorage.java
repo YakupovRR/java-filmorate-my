bd0baf9b6a9bd8859c1af27ffb321fc6143c6f39
Ruslan <ruslansamara00@mail.ru>
2022-09-26 22:58:02 +0400

    Испрвление тестов 3

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -71,11 +71,6 @@ public class UserDbStorage implements UserStorage {
     public void addFriend(int id, int friendId) {
         String sql = "INSERT INTO friends(user1_id, user2_id) VALUES (?, ?)";
         jdbcTemplate.update(sql, id, friendId);
-
-        String sql2= "INSERT INTO friends(user2_id, user1_id) VALUES (?, ?)";
-        jdbcTemplate.update(sql2, id, friendId);
-//        String sqlFriends2 = "SELECT user2_id FROM friends WHERE USER1_ID = ?";
-//        List<Integer> friends_userId2 = jdbcTemplate.queryForList(sqlFriends2, Integer.class, friendId);
     }
 
     @Override
